aa52e060e95301587a561196c05dfc91ceb77be0
Caua Martins <cauam8844@gmail.com>
2025-07-23 18:15:20 -0300

    quarto-commit

--- src/app/adiciona-contato/adiciona-contato.ts
+++ src/app/adiciona-contato/adiciona-contato.ts
@@ -1,17 +1,13 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
-import { CommonModule } from '@angular/common'; // 👈 importar isso!
-=======
->>>>>>> 2e9312ba0448fb8ec95f46e26cafe804bd89debe
+import { CommonModule } from '@angular/common';
 import { Contato } from './contato';
 
 @Component({
   selector: 'app-adiciona-contato',
-<<<<<<< HEAD
   standalone: true,
   templateUrl: './adiciona-contato.html',
   styleUrl: './adiciona-contato.css',
-  imports: [CommonModule] 
+  imports: [CommonModule]
 })
 export class AdicionaContato {
   contatos: Contato[];
@@ -20,27 +16,10 @@ export class AdicionaContato {
     this.contatos = [];
   }
 
-  adicionar(nm: string, tel: number, em: string, an: string, tipo: string) {
-    let aniversario = new Date(an);
-    const c = new Contato(nm, tel, em, aniversario, tipo);
-    this.contatos.push(c);
+ adicionar(nm: string, tel: number, em: string, an: string, tipo: string) {
+  let aniversario = new Date(an);
+  aniversario.setMinutes(aniversario.getMinutes() + aniversario.getTimezoneOffset());
+  const c = new Contato(nm, tel, em, aniversario, tipo);
+  this.contatos.push(c);
   }
 }
-=======
-  imports: [],
-  templateUrl: './adiciona-contato.html',
-  styleUrl: './adiciona-contato.css'
-})
-export class AdicionaContato {
-  contatos: Contato[]
-
-  constructor() {
-    this.contatos = []
-  }
-
-  adicionar(nm: string, tel:number, em:string, an: string, tipo: string) {
-    let aniversario = new Date(an)
-    const c = new Contato(nm, tel, em, aniversario, tipo)
-  }
-}
->>>>>>> 2e9312ba0448fb8ec95f46e26cafe804bd89debe
